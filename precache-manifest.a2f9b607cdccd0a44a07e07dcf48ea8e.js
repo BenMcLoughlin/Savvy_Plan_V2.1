7bf9ade906c0e22605dcad11fb17f8e5ba22005d
BenMcLoughlin <benmcl@shaw.ca>
2019-12-02 09:23:56 -0800

    Updates

---
+++ precache-manifest.a2f9b607cdccd0a44a07e07dcf48ea8e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea131572c2fc08ff4e6bd4090969f3dc",
+    "url": "/Savvy_Plan_V2.1/index.html"
+  },
+  {
+    "revision": "bfcfc4641c34ca7c36da",
+    "url": "/Savvy_Plan_V2.1/static/css/main.b6ecd049.chunk.css"
+  },
+  {
+    "revision": "86545a868bbb664949f8",
+    "url": "/Savvy_Plan_V2.1/static/js/2.0b0758bc.chunk.js"
+  },
+  {
+    "revision": "bfcfc4641c34ca7c36da",
+    "url": "/Savvy_Plan_V2.1/static/js/main.fcf50a34.chunk.js"
+  },
+  {
+    "revision": "f0e7bfd34c53a0635535",
+    "url": "/Savvy_Plan_V2.1/static/js/runtime~main.09246bde.js"
+  },
+  {
+    "revision": "77bd772d179326a33ca29342b4c7ed2c",
+    "url": "/Savvy_Plan_V2.1/static/media/SavvyPlan_logo_final_justlogo.77bd772d.svg"
+  },
+  {
+    "revision": "decc070f0109516de2120b3161088356",
+    "url": "/Savvy_Plan_V2.1/static/media/SavvyPlan_logo_final_white_justlogo.decc070f.svg"
+  },
+  {
+    "revision": "606139eda41896f680b7a7cf7f383438",
+    "url": "/Savvy_Plan_V2.1/static/media/contribution_plan.606139ed.png"
+  },
+  {
+    "revision": "3dff40fd14ca7b70399ea3f77d310442",
+    "url": "/Savvy_Plan_V2.1/static/media/goals_plan.3dff40fd.png"
+  },
+  {
+    "revision": "bea4a1b7f6d0bfce6166a17d413b7c81",
+    "url": "/Savvy_Plan_V2.1/static/media/savings_plan.bea4a1b7.png"
+  }
+]);
